def148dcb1aba86ac176d48da39cc82146842f47
LC9032 <lichieh.chou94@gmail.com>
2023-10-23 17:08:19 +0200

    commit

---
+++ src/CMakeFiles/aruco.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lc/CUDA_project/CU_aruco/src/apriltag_quad_thresh.cpp" "src/CMakeFiles/aruco.dir/apriltag_quad_thresh.cpp.o" "gcc" "src/CMakeFiles/aruco.dir/apriltag_quad_thresh.cpp.o.d"
+  "/home/lc/CUDA_project/CU_aruco/src/aruco.cpp" "src/CMakeFiles/aruco.dir/aruco.cpp.o" "gcc" "src/CMakeFiles/aruco.dir/aruco.cpp.o.d"
+  "/home/lc/CUDA_project/CU_aruco/src/zmaxheap.cpp" "src/CMakeFiles/aruco.dir/zmaxheap.cpp.o" "gcc" "src/CMakeFiles/aruco.dir/zmaxheap.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/cu_aruco.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for cu_aruco.
+# This may be replaced when dependencies are built.
