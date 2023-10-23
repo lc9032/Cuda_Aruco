d1cee2c7712667dfece691554ee05ba6c424cf4f
LC9032 <lichieh.chou94@gmail.com>
2023-10-23 17:00:40 +0200

    commit

---
+++ src/CMakeFiles/aruco.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aruco.dir/apriltag_quad_thresh.cpp.o"
+  "CMakeFiles/aruco.dir/apriltag_quad_thresh.cpp.o.d"
+  "CMakeFiles/aruco.dir/aruco.cpp.o"
+  "CMakeFiles/aruco.dir/aruco.cpp.o.d"
+  "CMakeFiles/aruco.dir/zmaxheap.cpp.o"
+  "CMakeFiles/aruco.dir/zmaxheap.cpp.o.d"
+  "libaruco.a"
+  "libaruco.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aruco.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/CMakeFiles/cu_aruco.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libcu_aruco.a"
+)
